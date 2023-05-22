99ee00e178faf43d9ffff5af30c5799ffef7b078
sandeepsharma2002 <sandeepsharmajec24@gmail.com>
2023-05-22 14:20:21 +0530

    second commit

--- src/components/Dashboard/Navbar/Navbar.js
+++ src/components/Dashboard/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -6,22 +6,23 @@ import { AiOutlineLogin } from "react-icons/ai";
 import user_img from "./user.png";
 
 export default function Navbar() {
+  const[Input_toggle, setinput] = useState(false);
   const { logout, isAuthenticated,user} = useAuth0();
   return (
     <div className="Navbar">
       <div className="Nav_heading">Dashboard</div>
       <div className="Nav_icons">
         <div class="search-box">
-          <input class="search-input" type="text" placeholder="Search..." />
-          <button class="search-btn">
+          <input class= { Input_toggle? "search-input show_input": "search-input"} type="text" placeholder="Search..." />
+          <button class="search-btn" onClick={ ()=> setinput(!Input_toggle)}>
             <i className="fa fa-fw fa-search fa-lg"></i>
           </button>
         </div>
-        <div className="bell">
+        <a className="bell Nav_logo">
           <i className="fa fa-fw fa-bell"></i>
-        </div>
+        </a>
         { 
-          isAuthenticated ? <button className="log_out_btn"
+          isAuthenticated ? <button className="log_out_btn Nav_logo" 
           onClick={() =>
             logout({ logoutParams: { returnTo: window.location.origin } })
           }
